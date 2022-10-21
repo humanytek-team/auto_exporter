a20ef4fe4b5b01ee5865c34d96d8b2926af6faa7
Moises Navarro <moisalejandro@gmail.com>
2022-10-21 15:18:27 -0500

    [ADD] fill_missing_fields

---
+++ auto_exporter/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Auto Exporter",
+    "version": "15.0.0.1.0",
+    "author": "HomebrewSoft",
+    "website": "https://homebrewsoft.dev",
+    "license": "LGPL-3",
+    "depends": [],
+    "data": [
+        # security
+        # data
+        # reports
+        # views
+    ],
+}
